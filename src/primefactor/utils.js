a749f03c6d6d61dcea37f879253525cf44f187f4
Victor Martin <victor.martin@42crunch.com>
2021-09-24 07:49:48 +0100

    Better implementation of prime factors for the use case

--- src/primefactor/utils.js
+++ src/primefactor/utils.js
@@ -2,26 +2,19 @@ const isValidNumber = (number) => {
   return Number.isInteger(number);
 };
 
-function getPrimeFactors(integer) {
-  const primeArray = [];
-  let isPrime;
+function getPrimeFactors(n) {
+  let factors = [],
+    divisor = 2;
 
-  // Find divisors starting with 2
-  for (let i = 2; i <= integer; i++) {
-    if (integer % i !== 0) continue;
-
-    // Check if the divisor is a prime number
-    for (let j = 2; j <= i / 2; j++) {
-      isPrime = i % j !== 0;
+  while (n > 2) {
+    if (n % divisor == 0) {
+      factors.push(divisor);
+      n = n / divisor;
+    } else {
+      divisor++;
     }
-
-    if (!isPrime) continue;
-    // if the divisor is prime, divide integer with the number and store it in the array
-    integer /= i;
-    primeArray.push(i);
   }
-
-  return primeArray;
+  return factors;
 }
 
 module.exports = { isValidNumber, getPrimeFactors };
